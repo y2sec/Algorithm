58b1d2c40b547d84759331e494f585499ad3cf84
y2sec <bae3835@gmail.com>
2020-12-08 23:42:02 +0900

    16769 Mixing Milk

--- Baekjoon/16769_Problem.py
+++ Baekjoon/16769_Problem.py
@@ -2,27 +2,23 @@
 
 milks = list()
 
+# 입력받은 우유를 리스트로 삽입
 for _ in range(3):
     milks.append(list(map(int, input().split())))
 
-curr_bottle = 0
-for _ in range(100):
-    if curr_bottle == 2:
-        milks[0][1] += milks[curr_bottle][1]
-        milks[curr_bottle][1] = 0
-        if milks[0][0] < milks[0][1]:
-            diff = milks[0][1] - milks[0][0]
-            milks[curr_bottle][1] += diff
-            milks[0][1] -= diff
-        curr_bottle = 0
-    else:
-        milks[curr_bottle+1][1] += milks[curr_bottle][1]
-        milks[curr_bottle][1] = 0
-        if milks[curr_bottle+1][0] < milks[curr_bottle+1][1]:
-            diff = milks[curr_bottle+1][1] - milks[curr_bottle+1][0]
-            milks[curr_bottle][1] += diff
-            milks[curr_bottle+1][1] -= diff
-        curr_bottle += 1
+# 100번을 반복
+for i in range(100):
+    # 현재 위치는 cur 다음 위치는 nxt
+    cur = i % 3
+    nxt = (i + 1) % 3
+    # 우유를 섞어줌
+    milks[nxt][1] += milks[cur][1]
+    milks[cur][1] = 0
+    # 넘치는 우유 양을 되돌림
+    if milks[nxt][0] < milks[nxt][1]:
+        diff = milks[nxt][1] - milks[nxt][0]
+        milks[cur][1] += diff
+        milks[nxt][1] -= diff
 
 for milk in milks:
     print(milk[1])
