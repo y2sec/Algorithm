64c4328b2e3199deafed072dce788c4ab3a3bd71
y2sec <bae3835@gmail.com>
2021-01-08 14:57:30 +0900

    1080 행렬

---
+++ Baekjoon/1080_Problem.py
@@ -0,0 +1,23 @@
+# 1080 행렬
+
+N, M = map(int, input().split())
+A = [[int(x) for x in input()] for _ in range(N)]
+B = [[int(x) for x in input()] for _ in range(N)]
+
+cnt = 0
+# 모든 자리를 비교해가며 뒤집음
+for i in range(N-2):
+    for j in range(M-2):
+        if A[i][j] != B[i][j]:
+            cnt += 1
+            for x in range(3):
+                for y in range(3):
+                    A[i+x][j+y] = (A[i+x][j+y] + 1) % 2
+
+# 그래도 두 행렬이 다르면 -1을 아니면 cnt를 출력
+for i in range(N):
+    for j in range(M):
+        if A[i][j] != B[i][j]:
+            print(-1)
+            exit()
+print(cnt)
