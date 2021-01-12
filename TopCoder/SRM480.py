3003a43e657c7a0b32ba341eb3e2e5fddbb0b1c1
y2sec <bae3835@gmail.com>
2021-01-12 16:02:26 +0900

    SRM480 Cryptography

---
+++ TopCoder/SRM480.py
@@ -0,0 +1,19 @@
+# SRM480 Cryptography
+
+def solution(numbers):
+    result = 0
+
+    for i in range(len(numbers)):
+        numbers[i] += 1
+        cnt = 1
+        for number in numbers:
+            cnt *= number
+        numbers[i] -= 1
+
+        result = max(result, cnt)
+
+    return result
+
+
+numbers = list(map(int, input().split()))
+print(solution(numbers))
