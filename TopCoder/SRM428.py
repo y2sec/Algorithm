4fc5ada2a7e39d0389aae9ecbc24fb93d2119b83
y2sec <bae3835@gmail.com>
2021-01-13 14:13:09 +0900

    SRM428 ThePalindrome

---
+++ TopCoder/SRM428.py
@@ -0,0 +1,19 @@
+# SRM428 ThePalindrome
+
+def solution(s):
+    start, end = 0, len(s)-1
+    lst = list(s)
+
+    while start <= end:
+        print(start, end)
+        if lst[start] != lst[end]:
+            lst.insert(end+1, lst[start])
+        else:
+            end -= 1
+        start += 1
+
+    print(lst)
+    print(len(lst))
+
+
+solution(input())
