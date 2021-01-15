0b38d08c94d85d6f7045afbfc7bb23e1238d9850
y2sec <bae3835@gmail.com>
2021-01-15 14:28:52 +0900

    SRM436 FriendScore

---
+++ TopCoder/SRM436.py
@@ -0,0 +1,23 @@
+# SRM436 FriendScore
+
+def solution(friends):
+    for i in range(N):
+        for j in range(N):
+            if i == j:
+                continue
+            if friends[i][j] == 'Y':
+                friend[i] += 1
+            else:
+                for k in range(N):
+                    if friends[i][k] == friends[j][k] and friends[i][k] == 'Y':
+                        friend[i] += 1
+                        break
+
+    return max(friend)
+
+
+N = int(input())
+friends = [input() for _ in range(N)]
+friend = [0 for _ in range(N)]
+
+print(solution(friends))
