6da04e66506fc5607af31e365dc5141fee67b5c7
y2sec <bae3835@gmail.com>
2021-01-20 15:30:51 +0900

    SRM363D2 HandShaking

---
+++ TopCoder/SRM363D2.py
@@ -0,0 +1,12 @@
+# SRM363D2 HandShaking
+
+n = 10
+
+handshaking = [0 for _ in range(n//2+1)]
+handshaking[0] = 1
+
+for i in range(1, n//2+1):
+    for j in range(i):
+        handshaking[i] += handshaking[j] * handshaking[i-j-1]
+
+print(handshaking[n//2])
