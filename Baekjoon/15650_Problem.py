e326f9d11a38dd4485123c6584f7a41b4b6c9dab
y2sec <bae3835@gmail.com>
2021-01-27 17:25:49 +0900

    15650 N과 M (2)

---
+++ Baekjoon/15650_Problem.py
@@ -0,0 +1,22 @@
+# 15650 N과 M (2)
+
+def sequence(num, cnt):
+    lst.append(num)
+    if cnt == M:
+        for i in lst:
+            print(i, end=' ')
+        print()
+        lst.pop()
+        return
+
+    for i in range(num+1, N+1):
+        if i not in lst:
+            sequence(i, cnt+1)
+    lst.pop()
+
+
+N, M = map(int, input().split())
+lst = []
+
+for n in range(1, N+1):
+    sequence(n, 1)
