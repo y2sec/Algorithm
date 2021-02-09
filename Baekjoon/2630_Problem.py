f25affa5f42bdec1ae020bb6339dc4d36fcaa3c1
y2sec <bae3835@gmail.com>
2021-02-09 14:04:25 +0900

    2630 색종이 만들기

---
+++ Baekjoon/2630_Problem.py
@@ -0,0 +1,40 @@
+# 2630 색종이 만들기
+
+import sys
+
+
+def chk(x, y, n):
+    standard = colorPaper[x][y]
+
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if colorPaper[i][j] != standard:
+                return False
+
+    return True
+
+
+def cuting(x, y, n):
+    global white, blue
+    if n < 1:
+        return
+    if chk(x, y, n):
+        if colorPaper[x][y] == 1:
+            blue += 1
+        else:
+            white += 1
+        return
+    cuting(x, y, n//2)
+    cuting(x, y+(n//2), n//2)
+    cuting(x+(n//2), y, n//2)
+    cuting(x+(n//2), y+(n//2), n//2)
+
+
+N = int(sys.stdin.readline())
+colorPaper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+white = 0
+blue = 0
+cuting(0, 0, N)
+
+print(white)
+print(blue)
