61b4d6dc9aa884c613aca3bfd17b9d7fb6c35ba7
y2sec <bae3835@gmail.com>
2021-06-04 17:31:05 +0900

    9205 맥주 마시면서 걸어가기

--- Baekjoon/9205_Problem.py
+++ Baekjoon/9205_Problem.py
@@ -9,7 +9,7 @@ for _ in range(t):
     n = int(sys.stdin.readline())
 
     locations = []
-    dist = [[float('inf') for _ in range(n + 2)] for _ in range(n + 2)]
+    dist = [[False for _ in range(n + 2)] for _ in range(n + 2)]
 
     for i in range(n + 2):
         x, y = map(int, sys.stdin.readline().split())
@@ -19,15 +19,15 @@ for _ in range(t):
         for j in range(n + 2):
             diff = abs(locations[i][0] - locations[j][0]) + abs(locations[i][1] - locations[j][1])
             if diff <= 1000:
-                dist[i][j] = 0
+                dist[i][j] = True
 
     for k in range(n + 2):
         for i in range(n + 2):
             for j in range(n + 2):
-                if dist[i][k] == 0 and dist[k][j] == 0:
-                    dist[i][j] = 0
+                if dist[i][k] and dist[k][j]:
+                    dist[i][j] = True
 
-    if dist[0][-1] == 0:
+    if dist[0][-1]:
         print('happy')
     else:
         print('sad')
