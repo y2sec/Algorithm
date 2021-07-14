55f6c42a359f476a6f1eb72df7f5047eb534e402
y2sec <bae3835@gmail.com>
2021-07-14 21:34:37 +0900

    디스크 컨트롤러

---
+++ Programmers/64.py
@@ -0,0 +1,32 @@
+# 디스크 컨트롤러
+import heapq
+
+
+def solution(jobs):
+    answer = 0
+
+    countJob = len(jobs)
+
+    heapq.heapify(jobs)
+    jobHeap = []
+
+    req, time = heapq.heappop(jobs)
+    ms = req
+    heapq.heappush(jobHeap, [time, req])
+
+    while jobHeap:
+        time, req = heapq.heappop(jobHeap)
+        answer += (ms + time) - req
+        ms += time
+
+        if jobs and not jobHeap and ms < jobs[0][0]:
+            ms = jobs[0][0]
+
+        while jobs and ms >= jobs[0][0]:
+            req, time = heapq.heappop(jobs)
+            heapq.heappush(jobHeap, [time, req])
+
+    return answer // countJob
+
+
+print(solution([[0, 3], [1, 9], [2, 6]]))
